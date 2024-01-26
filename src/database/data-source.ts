083d38de5a61a1f419c5d2ba8c5e1b791272cc86
AkanWealth <akan.asanga@gmail.com>
2024-01-26 19:22:26 +0100

    changes in env

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,14 +5,20 @@ dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || 'mydb',
+  // host: process.env.DB_HOST,
+  // port: parseInt(process.env.DB_PORT, 10),
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_STRING,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
   synchronize: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 const dataSource = new DataSource(dataSourceOptions);
